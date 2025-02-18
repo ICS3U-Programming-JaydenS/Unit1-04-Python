f152ce32cf5211bf3d1207e5dc64b4093da398da
Jayden Smith <jayden.smith2@stu.ocsb.ca>
2025-02-18 18:00:07 +0000

    Added basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created By: Jayden Smith
+
+# Date: February 18, 2025
+
+# This program does basic math.
+
+import math;
+
+def main():
+    print("33+3={}".format(33 + 3))
+    print("56-3={}".format(56 - 3))
+    print("8*8={}".format(8 * 8))
+    print("42334/2412={}".format(42334 / 2412))
+    print("2**12= {}".format(2**12))
+    print(" √9={}". format (math.sqrt(9)))
+
+
+if __name__ == "__main__":
+    main()
